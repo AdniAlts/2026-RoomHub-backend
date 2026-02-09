b75ad6f0292379f7fd29c83f1929dc6dc8803fee
AdniAlts <abubakaradni16@gmail.com>
2026-02-10 00:23:02 +0700

    feat: add seed data for 30 rooms

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -26,5 +26,39 @@ public class AppDbContext : DbContext
             .WithMany()
             .HasForeignKey(b => b.RoomId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        // Seed Data untuk Rooms
+        modelBuilder.Entity<Room>().HasData(
+            new Room { Id = 1, RoomCode = "LK-101", RoomName = "Lab Komputer 1", Capacity = 40, Location = "Gedung A Lantai 1" },
+            new Room { Id = 2, RoomCode = "LK-102", RoomName = "Lab Komputer 2", Capacity = 40, Location = "Gedung A Lantai 1" },
+            new Room { Id = 3, RoomCode = "LK-201", RoomName = "Lab Komputer 3", Capacity = 35, Location = "Gedung A Lantai 2" },
+            new Room { Id = 4, RoomCode = "RK-101", RoomName = "Ruang Kelas 101", Capacity = 50, Location = "Gedung B Lantai 1" },
+            new Room { Id = 5, RoomCode = "RK-102", RoomName = "Ruang Kelas 102", Capacity = 50, Location = "Gedung B Lantai 1" },
+            new Room { Id = 6, RoomCode = "RK-103", RoomName = "Ruang Kelas 103", Capacity = 45, Location = "Gedung B Lantai 1" },
+            new Room { Id = 7, RoomCode = "RK-201", RoomName = "Ruang Kelas 201", Capacity = 50, Location = "Gedung B Lantai 2" },
+            new Room { Id = 8, RoomCode = "RK-202", RoomName = "Ruang Kelas 202", Capacity = 50, Location = "Gedung B Lantai 2" },
+            new Room { Id = 9, RoomCode = "RK-203", RoomName = "Ruang Kelas 203", Capacity = 45, Location = "Gedung B Lantai 2" },
+            new Room { Id = 10, RoomCode = "RK-301", RoomName = "Ruang Kelas 301", Capacity = 40, Location = "Gedung B Lantai 3" },
+            new Room { Id = 11, RoomCode = "RM-101", RoomName = "Ruang Meeting 1", Capacity = 20, Location = "Gedung C Lantai 1" },
+            new Room { Id = 12, RoomCode = "RM-102", RoomName = "Ruang Meeting 2", Capacity = 15, Location = "Gedung C Lantai 1" },
+            new Room { Id = 13, RoomCode = "RM-201", RoomName = "Ruang Meeting 3", Capacity = 25, Location = "Gedung C Lantai 2" },
+            new Room { Id = 14, RoomCode = "AUD-01", RoomName = "Auditorium Utama", Capacity = 300, Location = "Gedung D" },
+            new Room { Id = 15, RoomCode = "AUD-02", RoomName = "Auditorium Mini", Capacity = 150, Location = "Gedung D" },
+            new Room { Id = 16, RoomCode = "LB-101", RoomName = "Lab Bahasa", Capacity = 30, Location = "Gedung E Lantai 1" },
+            new Room { Id = 17, RoomCode = "LF-101", RoomName = "Lab Fisika 1", Capacity = 30, Location = "Gedung F Lantai 1" },
+            new Room { Id = 18, RoomCode = "LF-102", RoomName = "Lab Fisika 2", Capacity = 30, Location = "Gedung F Lantai 1" },
+            new Room { Id = 19, RoomCode = "LK-103", RoomName = "Lab Kimia", Capacity = 25, Location = "Gedung F Lantai 2" },
+            new Room { Id = 20, RoomCode = "ST-101", RoomName = "Studio Multimedia", Capacity = 20, Location = "Gedung G Lantai 1" },
+            new Room { Id = 21, RoomCode = "ST-102", RoomName = "Studio Recording", Capacity = 15, Location = "Gedung G Lantai 1" },
+            new Room { Id = 22, RoomCode = "RK-104", RoomName = "Ruang Kelas 104", Capacity = 48, Location = "Gedung B Lantai 1" },
+            new Room { Id = 23, RoomCode = "RK-204", RoomName = "Ruang Kelas 204", Capacity = 48, Location = "Gedung B Lantai 2" },
+            new Room { Id = 24, RoomCode = "RK-302", RoomName = "Ruang Kelas 302", Capacity = 40, Location = "Gedung B Lantai 3" },
+            new Room { Id = 25, RoomCode = "LP-101", RoomName = "Lab Pemrograman", Capacity = 35, Location = "Gedung A Lantai 2" },
+            new Room { Id = 26, RoomCode = "LJ-101", RoomName = "Lab Jaringan", Capacity = 30, Location = "Gedung A Lantai 3" },
+            new Room { Id = 27, RoomCode = "RS-101", RoomName = "Ruang Seminar 1", Capacity = 100, Location = "Gedung H Lantai 1" },
+            new Room { Id = 28, RoomCode = "RS-102", RoomName = "Ruang Seminar 2", Capacity = 80, Location = "Gedung H Lantai 2" },
+            new Room { Id = 29, RoomCode = "RM-103", RoomName = "Ruang Meeting 4", Capacity = 12, Location = "Gedung C Lantai 2" },
+            new Room { Id = 30, RoomCode = "LD-101", RoomName = "Lab Desain Grafis", Capacity = 25, Location = "Gedung G Lantai 2" }
+        );
     }
 }
--- Migrations/20260209172026_SeedRoomData.Designer.cs
+++ Migrations/20260209172026_SeedRoomData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace _2026_RoomHub_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260209172026_SeedRoomData")]
+    partial class SeedRoomData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
